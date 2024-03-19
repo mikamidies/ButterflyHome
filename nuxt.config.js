0f7884859c8c7360023bee09409d0f777b607a1d
mikamidies <mikamidies04@gmail.com>
2024-03-19 08:06:49 +0500

    update

---
+++ nuxt.config.js
@@ -0,0 +1,51 @@
+export default {
+  head: {
+    title: "ButterflyHome",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  css: ["ant-design-vue/dist/antd.css", "@/assets/css/main.css"],
+
+  plugins: ["@/plugins/antd-ui"],
+
+  components: true,
+
+  buildModules: [],
+
+  modules: ["@nuxtjs/axios"],
+
+  axios: {
+    baseURL: "/",
+  },
+
+  build: {
+    babel: {
+      compact: true,
+    },
+    standalone: true,
+    extend(config, ctx) {
+      config.externals = [
+        {
+          encoding: "encoding",
+        },
+      ];
+    },
+  },
+
+  i18n: {
+    locales: ["en", "ru"],
+    defaultLocale: "en",
+    vueI18n: {
+      fallbackLocale: "en",
+    },
+  },
+};
