50c98e1c797abbccfd42b6f5b295725c4c42e317
mikamidies <mikamidies04@gmail.com>
2024-03-19 08:48:55 +0500

    update

--- components/DesktopHeader.vue
+++ components/DesktopHeader.vue
@@ -1,9 +1,140 @@
 <template>
-  <div class="wrap"></div>
+  <div class="wrap">
+    <div class="container">
+      <div class="left">
+        <NuxtLink to="/" class="brand">
+          <img src="@/assets/img/logo/brand.svg" alt="" />
+        </NuxtLink>
+      </div>
+      <div class="mid">
+        <ul class="links">
+          <li>
+            <NuxtLink to="/"> Главная </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/"> Продукты </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/"> Услуги </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/"> О нас </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/"> Контакты </NuxtLink>
+          </li>
+        </ul>
+      </div>
+      <div class="right">
+        <div class="socials">
+          <a href="#">
+            <TelegramIcon />
+          </a>
+          <a href="#">
+            <WhatsappIcon />
+          </a>
+        </div>
+        <div class="number">
+          <a href="#"> +971 52 246 40 48 </a>
+        </div>
+        <div class="lang">
+          <a-dropdown>
+            <a-menu slot="overlay" @click="handleMenuClick">
+              <a-menu-item key="1">
+                <RuFlag />
+                <p>Ру</p>
+              </a-menu-item>
+            </a-menu>
+            <a-button style="margin-left: 8px">
+              <RuFlag />
+              <p>Ру</p>
+              <ChevronDown />
+            </a-button>
+          </a-dropdown>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import RuFlag from "./SvgIcons/RuFlag.vue";
+import UzbFlag from "./SvgIcons/UzbFlag.vue";
+import ChevronDown from "./SvgIcons/ChevronDown.vue";
+import TelegramIcon from "./SvgIcons/TelegramIcon.vue";
+import WhatsappIcon from "./SvgIcons/WhatsappIcon.vue";
+
+export default {
+  components: {
+    TelegramIcon,
+    WhatsappIcon,
+    RuFlag,
+    ChevronDown,
+    UzbFlag,
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.wrap {
+  padding: 12px 0;
+}
+.container {
+  display: grid;
+  align-items: center;
+  grid-template-columns: 1fr 5fr 4fr;
+}
+.brand {
+  display: block;
+}
+.mid {
+  display: flex;
+  justify-content: center;
+}
+.links {
+  display: flex;
+  align-items: center;
+  gap: 40px;
+}
+.links a {
+  color: var(--Black, #020105);
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%; /* 27px */
+}
+.right {
+  display: flex;
+  align-items: center;
+  gap: 48px;
+}
+.socials {
+  display: flex;
+  align-items: center;
+  gap: 16px;
+}
+.socials a {
+  display: flex;
+}
+.number a {
+  color: var(--Green, #009a10);
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%; /* 30px */
+  text-decoration-line: underline;
+}
+.wrap :deep(.ant-btn) {
+  margin-left: 0 !important;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  border: 0;
+  padding: 0;
+  box-shadow: none;
+}
+.right {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
---
+++ components/SvgIcons/RuFlag.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="24"
+    viewBox="0 0 32 24"
+    fill="none"
+  >
+    <g clip-path="url(#clip0_85_4746)">
+      <rect y="0.5" width="32" height="23" rx="3" fill="#1A47B8" />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M0 15.8333H32V23.4999H0V15.8333Z"
+        fill="#F93939"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M0 0.5H32V8.16667H0V0.5Z"
+        fill="white"
+      />
+    </g>
+    <rect x="0.5" y="1" width="31" height="22" rx="2.5" stroke="#E7EDFB" />
+    <defs>
+      <clipPath id="clip0_85_4746">
+        <rect y="0.5" width="32" height="23" rx="3" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
