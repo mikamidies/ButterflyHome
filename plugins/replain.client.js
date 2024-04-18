cb3ce96e85fa6406c0f78ee5a7736da022dcd116
mikamidies <mikamidies04@gmail.com>
2024-04-18 10:50:42 +0500

    qul

---
+++ plugins/replain.client.js
@@ -0,0 +1,8 @@
+window.replainSettings = { id: "27d5f757-0a53-4f3f-9afb-056b07da0bae" };
+(function (u) {
+  var s = document.createElement("script");
+  s.async = true;
+  s.src = u;
+  var x = document.getElementsByTagName("script")[0];
+  x.parentNode.insertBefore(s, x);
+})("https://widget.replain.cc/dist/client.js");
